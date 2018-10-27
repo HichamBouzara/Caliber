05edaf92edfe7fe445eb016e8eb46a1be63bca85
Abderraouf Safsaf <fa_safsaf@esi.dz>
2018-10-27 03:48:41 +0200

    Working

--- public/js/workgroups.js
+++ public/js/workgroups.js
@@ -2,6 +2,7 @@ $('#createWorkgroup').click(() => {
     var workgroup = {
         "title": $('#workgroupTitle').val(),
         "penality": $('#challengeTitle').val(),
+        "users": ["5bd243929ef18c132cd73580"]
     }
     $.post('/workgroups', { "workgroup": workgroup }, (c) => {
         location.reload();
